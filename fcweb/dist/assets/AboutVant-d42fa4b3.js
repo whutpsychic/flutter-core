bd23d8d16ff8f7bae41253b503b4b7e6d3c1845d
zbc <1511828529@qq.com>
2023-05-12 11:27:14 +0800

    dist

---
+++ fcweb/dist/assets/AboutVant-d42fa4b3.js
@@ -0,0 +1,2 @@
+import{_ as t,o as e,k as o}from"./index-0f0715f6.js";const n={},r={src:"https://vant-contrib.gitee.io/vant/mobile.html#/zh-CN#/zh-CN/quickstart"};function s(c,a,i,_,p,f){return e(),o("iframe",r,`\r
+  `)}const h=t(n,[["render",s]]);export{h as default};
